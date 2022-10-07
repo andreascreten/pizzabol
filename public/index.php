709d8de95b84b3933f0ecf9f3293fd27beece605
Andreas Creten <andreas@madewithlove.be>
2022-10-07 14:02:57 +0200

    Pizzabol website

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Pizzabol.com</title>
+</head>
+<body>
+<div class="relative rounded-xl overflow-auto p-8 text-center">
+    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-4">
+        <span class="block">Pizzabol.com</span>
+        <span class="block text-red-600 text-2xl">Start your free trial today.</span>
+    </h2>
+    <div class="rounded-lg overflow-hidden w-56 sm:w-96 mx-auto">
+        <img class="object-cover h-48 w-full " src="pizzabol.jpeg">
+    </div>
+</div>
+</body>
+</html>
